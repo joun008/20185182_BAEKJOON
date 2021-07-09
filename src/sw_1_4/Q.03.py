93e34cf6bb10f828c78ccfc5afc3952c5f3ef311
joun008 <80435767+joun008@users.noreply.github.com>
2021-07-10 01:26:10 +0900

    Create Q.03.py

---
+++ src/sw_1_4/Q.03.py
@@ -0,0 +1,25 @@
+#11724 이분 탐색
+import sys
+input =sys.stdin.readline
+sys.setrecursionlimit(10000)
+N,M = map(int,input().split())
+arr = [[]for i in range(N+1)]
+for i in range(M) :
+    a,b = map(int,input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+visited = []
+def dfs(start) :
+    visited.append(start)
+    for i in arr[start] :
+        if i not in visited :
+            dfs(i)
+    return visited
+count=0
+
+for j in range(1,N+1) :
+    if j not in visited : 
+        count+=1
+        dfs(j)
+print(count)
